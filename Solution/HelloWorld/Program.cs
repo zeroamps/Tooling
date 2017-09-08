0973b7f8f431be1c4178e85645e4489af84e56e1
Jozef Chovan <jozef.chovan@gmail.com>
2017-09-08 11:19:55 +0200

    A new solution created with a console application.

---
+++ Solution/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HelloWorld
+{
+    using System;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
